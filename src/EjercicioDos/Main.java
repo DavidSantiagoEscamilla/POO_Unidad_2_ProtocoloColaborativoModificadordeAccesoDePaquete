a0a8c29af192e791126ea7a5f1003df14a383660
sergiolvargas95 <sergiolvargas95@gmail.com>
2024-11-24 16:29:48 -0500

    ejercicio dos

--- src/EjercicioDos/Main.java
+++ src/EjercicioDos/Main.java
@@ -6,6 +6,6 @@ public class Main {
     public static void main(String[] args) {
         Vehiculo mVehiculo = new Vehiculo("Carro");
 
-        Moto moto = new Moto("Moto");
+        //Moto moto = new Moto("Moto");
     }
 }
