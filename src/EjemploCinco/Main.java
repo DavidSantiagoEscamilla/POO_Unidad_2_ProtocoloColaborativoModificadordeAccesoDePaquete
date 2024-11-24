4c8ffcac932e39d804ab99115f5ea06d3a8878b0
sergiolvargas95 <sergiolvargas95@gmail.com>
2024-11-24 16:40:15 -0500

    ejemplos cinco

--- src/EjemploCinco/Main.java
+++
@@ -1,7 +0,0 @@
-package EjemploCinco;
-
-public class Main {
-    public static void main(String[] args) {
-        Vehiculo v = new Vehiculo("Toyota"); // Error de compilación: Vehiculo no es visible
-    }
-}
---
+++ src/EjemploCinco/paqueteB/Main.java
@@ -0,0 +1,7 @@
+package EjemploCinco;
+
+public class Main {
+    public static void main(String[] args) {
+        Vehiculo vehiculo = new Vehiculo("Toyota"); // Error de compilación: Vehiculo no es visible
+    }
+}
